ff56781a6d422c466f289b0e1ef9442ad193bd65
udirona <>
2024-10-12 08:40:51 +0200

    switch to qmk wsl

--- draw_keyboard_layout.sh
+++ draw_keyboard_layout.sh
@@ -1,12 +1,17 @@
-# qmk via2json -kb crkbd ../keyboard/corne/crkbd.layout.json 
-# keymap parse -c 10 -q - > ../keyboard/corne/via_layout.yaml
-# keymap draw ../keyboard/corne/via_layout.yaml > ../keyboard/corne/via.svg
-# qmk via2json -kb crkbd ../keyboard/corne/crkbd.layout.json | keymap parse -c 10 -q - > ../keyboard/corne/crkbd.layout.yaml
-# keymap draw ../keyboard/corne/crkbd.layout.yaml > ../keyboard/corne/crkbd.layout.svg
-# qmk via2json -kb iris ../keyboard/iris/iris.layout.json | keymap parse -c 10 -q - > ../keyboard/iris/crkbd.layout.yaml
-# keymap draw ../keyboard/iris/crkbd.layout.yaml > ../keyboard/iris/crkbd.layout.svg
+# maybe this could more elegant ... but ..
 
-qmk via2json -kb crkbd ../keyboard/corne/crkbd.layout.json | keymap -c ../keyboard/keymap_drawer.config.yaml parse -c 10 -q - > ../keyboard/corne/crkbd.layout.yaml
-keymap -c ../keyboard/keymap_drawer.config.yaml draw ../keyboard/corne/crkbd.layout.yaml > ../keyboard/corne/crkbd.svg
-qmk via2json -kb keebio/iris/rev7 ../keyboard/iris/iris_rev__7.layout.json | keymap -c ../keyboard/keymap_drawer.config.yaml parse -c 10 -q - > ../keyboard/iris/iris.layout.yaml
-keymap -c ../keyboard/keymap_drawer.config.yaml draw ../keyboard/iris/iris.layout.yaml > ../keyboard/iris/iris.svg
+qmk via2json -kb crkbd corne/crkbd.layout.json > ./corne/crkbd.via2json.json
+keymap -c keymap_drawer.config.yaml parse -q ./corne/crkbd.via2json.json -c 10 -o ./corne/crkbd.layout.yaml
+keymap -c keymap_drawer.config.yaml draw ./corne/crkbd.layout.yaml -o ./corne/crkbd.svg
+
+
+qmk via2json -kb keebio/iris/rev7 iris/iris_rev__7.layout.json > ./iris/iris.via2json.json
+keymap -c keymap_drawer.config.yaml parse -q ./iris/iris.via2json.json -c 10 -o ./iris/iris.layout.yaml
+keymap -c keymap_drawer.config.yaml draw ./iris/iris.layout.yaml -o ./iris/iris.svg
+
+# clen up temp
+rm ./iris/iris.via2json.json
+rm ./iris/iris.layout.yaml
+
+rm ./corne/crkbd.via2json.json
+rm ./corne/crkbd.layout.yaml
